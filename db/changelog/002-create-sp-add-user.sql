b70ffdbc834d48561ce33c22039e49e3ff5d0510
vovantung <vovantungdt123@gmail.com>
2025-10-01 10:49:22 +0700

    cm25

--- db/changelog/002-create-sp-add-user.sql
+++ db/changelog/002-create-sp-add-user.sql
@@ -1,13 +1,26 @@
---liquibase formatted sql
-
---changeset dev:002-create-sp-add-user
-CREATE OR REPLACE PROCEDURE sp_add_user (
-    p_username   IN VARCHAR2,
-    p_email      IN VARCHAR2
+CREATE OR REPLACE PROCEDURE get_related_tasks (
+    p_user_id IN NUMBER,
+    p_result OUT SYS_REFCURSOR
 ) AS
 BEGIN
-INSERT INTO users (username, email, created_at)
-VALUES (p_username, p_email, SYSDATE);
+OPEN p_result FOR
+SELECT t.*,
+       CASE WHEN wc.min_ln = 1 THEN 0 ELSE 1 END AS vaitro
+FROM task t
+         JOIN (
+    -- chỉ những dòng thuộc prefix liên tục (level_number = rn) và user = p_user_id
+    SELECT x.workflow_id,
+           MIN(x.level_number) AS min_ln
+    FROM (
+             SELECT wl.workflow_id,
+                    wl.user_id,
+                    wl.level_number,
+                    ROW_NUMBER() OVER (PARTITION BY wl.workflow_id ORDER BY wl.level_number) AS rn
+             FROM workflow_level wl
+         ) x
+    WHERE x.level_number = x.rn
+      AND x.user_id = p_user_id
+    GROUP BY x.workflow_id
+) wc
+              ON wc.workflow_id = t.workflow_id;
 END;
-/
---rollback DROP PROCEDURE sp_add_user;
